9aec9053a3fa02b0c0146a6e6a596a49c51756e7
rohitmane9022 <rohitmane8356@gmail.com>
2025-01-24 11:32:35 +0530

    intial

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,18 @@
+
+'use client'
+
 import ProjectsList from '@/components/ProjectsList'
+import { fetchProjects } from '@/redux/projectsSlice'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 export default function ProjectsPage() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(fetchProjects())
+  },[dispatch])
+
   return (
     <div className="space-y-6">
       <h1 className="text-3xl font-bold">Projects</h1>
--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,6 +1,16 @@
+'use client'
+
 import TeamList from '@/components/TeamList'
+import { fetchTeamMembers } from '@/redux/teamSlice'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 export default function TeamPage() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(fetchTeamMembers())
+  },[])
   return (
     <div className="space-y-6">
       <h1 className="text-3xl font-bold">Team Members</h1>
